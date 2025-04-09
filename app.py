91715b17ab7f6694ec232584603c34cbdb3bcf28
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-04-09 15:01:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ elif page == "Fit Gaussians to Data":
     st.title("Fit Gaussians to Data")
     
     import upload_plot  # This will bring in the CSV upload functionality from upload_plot.py
-    upload_plot.upload_and_plot()
     
 # Upload CSV Page (handles the file upload)
 elif page == "Calibrate (IMSCal)":
