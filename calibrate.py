a5a1c4848f4a655890d9d4707b0adbeb9389bda9
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-04-09 16:24:08 +0100

    Update calibrate.py

--- calibrate.py
+++ calibrate.py
@@ -3,12 +3,12 @@ import streamlit as st
 # Streamlit UI
 st.title("Simple Streamlit App")
 
-# Create a checkbox
-user_input = st.checkbox("Try me")
-
-# Check if the checkbox is ticked
-if user_input:
+# Create a checkbox and store its state
+if st.checkbox("Try me"):
     st.write("Working")
+else:
+    st.write("Please try me!")
+
 
 
 
