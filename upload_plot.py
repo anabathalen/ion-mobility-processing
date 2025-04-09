04f88d938941f71ec5101057f9c584863de8855e
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-04-09 11:22:58 +0100

    Update upload_plot.py

--- upload_plot.py
+++ upload_plot.py
@@ -55,7 +55,7 @@ def upload_and_plot():
                 initial_guess = []
                 for peak in peaks:
                     # Initial guess: amplitude (max y), mean (user input), and standard deviation (arbitrary, set to 1)
-                    initial_guess += [max(y_data), peak, 1]
+                    initial_guess += [max(y_data), peak, 10]
 
                 # Fitting function to include only a local region (x-10 to x+10)
                 def multi_gaussian(x, *params):
